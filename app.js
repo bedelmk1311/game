7ed1b85dd82427404c46862749168944c083b148
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-06-20 17:52:12 +0000

    [add]敵とアイテム

--- app.js
+++ app.js
@@ -10,3 +10,13 @@ document.addEventListener('keydown', (e) => {
   player.style.left = x + 'px';
   player.style.top = y + 'px';
 });
+
+// 敵・アイテムを出現させる
+function createItem() {
+  const item = document.createElement('div');
+  item.classList.add('item');
+  item.style.left = Math.random() * window.innerWidth + 'px';
+  item.style.top = '-40px';
+  document.body.appendChild(item);
+}
+setInterval(createItem, 1000); // 1秒ごとに生成
